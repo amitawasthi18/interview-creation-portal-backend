d597db0abd029d1740046b378d8552a3303a457a
Amit Awasthi <91770867+amitawasthi18@users.noreply.github.com>
2022-12-04 22:03:51 +0530

    resolved cors error

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
 mongoose.connect(process.env.MONGOURI, {
   useNewUrlParser: true,
@@ -20,6 +21,7 @@ mongoose.connection.on("error", (err) => {
 require("./models/user");
 require("./models/meeting");
 app.use(express.json());
+app.use(cors());
 
 
 app.use(require("./routes/user"));
